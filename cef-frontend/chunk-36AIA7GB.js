78e745aa1b31534a3ca42f16f8e7b684b4d6c748
moonslayers <moonslayers@users.noreply.github.com>
2026-02-17 19:08:25 +0000

    Update cef frontend build

--- cef-frontend/chunk-36AIA7GB.js
+++
@@ -1,2 +0,0 @@
-import"./chunk-KFUIWQMZ.js";var o=[{path:"",redirectTo:"inicio",pathMatch:"full"},{path:"inicio",loadComponent:()=>import("./chunk-YLB2HK2Z.js").then(t=>t.InicioComponent)},{path:"usuarios",title:"Usuarios",loadChildren:()=>import("./chunk-D2CCC64Y.js").then(t=>t.usuariosRoutes)},{path:"externos",title:"Externos",loadChildren:()=>import("./chunk-KCQ56HQ5.js").then(t=>t.externosRoutes)}];export{o as mainRoutes};
-//# sourceMappingURL=chunk-36AIA7GB.js.map
---
+++ cef-frontend/chunk-ZFJNWZKF.js
@@ -0,0 +1,2 @@
+import"./chunk-JKATPHND.js";var e=[];export{e as externosRoutes};
+//# sourceMappingURL=chunk-ZFJNWZKF.js.map
