92ec8cd9d5b69665d1b64fe88451817075e90a42
moonslayers <moonslayers@users.noreply.github.com>
2026-02-23 17:44:06 +0000

    Update sagem frontend build

--- sagem-frontend/chunk-7TBW7N3J.js
+++
@@ -1,2 +0,0 @@
-import"./chunk-UTAY5AUX.js";var o=[{path:"",redirectTo:"inicio",pathMatch:"full"},{path:"inicio",loadComponent:()=>import("./chunk-UYZTTXLC.js").then(t=>t.WelcomeComponent)},{path:"usuarios",title:"Usuarios",loadChildren:()=>import("./chunk-CVNVI7WV.js").then(t=>t.usuariosRoutes)},{path:"solicitudes",title:"Solicitudes",loadChildren:()=>import("./chunk-2S52XI7E.js").then(t=>t.solicitudesRoutes)},{path:"catalogos",title:"Cat\xE1logos",loadChildren:()=>import("./chunk-GDFYD3PS.js").then(t=>t.catalogosRoutes)}];export{o as mainRoutes};
-//# sourceMappingURL=chunk-7TBW7N3J.js.map
