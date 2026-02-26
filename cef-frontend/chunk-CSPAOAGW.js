82792765d1a52ca87765745f7cef1861d5eb1cc7
moonslayers <moonslayers@users.noreply.github.com>
2026-02-26 19:12:28 +0000

    Update cef frontend build

---
+++ cef-frontend/chunk-CSPAOAGW.js
@@ -0,0 +1,2 @@
+var a={app_name:"Secretar\xEDa de Econom\xEDa e Innovaci\xF3n",app_prefix:"CEF",api_url:"https://sei-apps.bajacalifornia.gob.mx/cef",logo_name:"main_logo.png"};export{a};
+//# sourceMappingURL=chunk-CSPAOAGW.js.map
