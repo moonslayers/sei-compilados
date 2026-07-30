b8cc4a53d15972389b207872cd3ee13a25fd6bad
moonslayers <moonslayers@users.noreply.github.com>
2026-07-30 22:18:05 +0000

    Update vdin frontend build

--- vdin-ui/chunk-DuxJq_Fr.js
+++
@@ -1,9 +0,0 @@
-import {p,a2 as Ee,ba as To,bb as x,bc as ye,bd as wo,_ as _l,be as k}from'./main.js';function q(t){t||(t=p(Ee));let r=new x(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(To(r))}function E(t,r){let e=p(ye),n=new wo(1),v=_l(()=>{let u;try{u=t();}catch(f){k(()=>n.error(f));return}k(()=>n.next(u));},{injector:e,manualCleanup:true});return e.get(Ee).onDestroy(()=>{v.destroy(),n.complete();}),n.asObservable()}/*! Bundled license information:
-
-@angular/core/fesm2022/rxjs-interop.mjs:
-  (**
-   * @license Angular v22.0.5
-   * (c) 2010-2026 Google LLC. https://angular.dev/
-   * License: MIT
-   *)
-*/export{E,q};//# sourceMappingURL=chunk-DuxJq_Fr.js.map
